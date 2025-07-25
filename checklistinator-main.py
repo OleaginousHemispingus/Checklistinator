91e98186a40d561963f236f724d3967419c5788a
Joshua Lucas Johnson <124935024+OleaginousHemispingus@users.noreply.github.com>
2025-07-25 16:27:48 -0400

    Update checklistinator-main.py

--- checklistinator-main.py
+++ checklistinator-main.py
@@ -724,8 +724,8 @@ st.cache_data.clear()
 snapshot = tracemalloc.take_snapshot()
 top_stats = snapshot.statistics('lineno')
 
-print("[ Top 10 memory-consuming lines ]")
+st.write(" Top 10 memory-consuming lines ")
 for stat in top_stats[:10]:
-	print(stat)
+	st.write(stat)
 
 tracemalloc.stop()
