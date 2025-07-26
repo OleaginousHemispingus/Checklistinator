bee2dcffb2d6ec8eacb8f928bb78217eea3e673c
Joshua Lucas Johnson <124935024+OleaginousHemispingus@users.noreply.github.com>
2025-07-26 16:07:54 -0400

    Update checklistinator-main.py

--- checklistinator-main.py
+++ checklistinator-main.py
@@ -539,8 +539,7 @@ def get_place(place_str: str):
 		st.write(len(matching_files))
 		for f in matching_files:
 			st.write(f)
-			path = os.path.join('birds-data/checklistinator', f)
-			area_lists.append(path)
+			area_lists.append(f)
 #			st.write("appended!")
 		big_area(area_lists)
 	else:
