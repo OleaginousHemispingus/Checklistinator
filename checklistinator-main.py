afb442867142443671ca9c90158722ca09e4ba07
Joshua Lucas Johnson <124935024+OleaginousHemispingus@users.noreply.github.com>
2025-07-26 16:06:39 -0400

    Update checklistinator-main.py

--- checklistinator-main.py
+++ checklistinator-main.py
@@ -538,7 +538,7 @@ def get_place(place_str: str):
 		matching_files = [f for f in fs.ls("birds-data/checklistinator") if pattern.match(os.path.basename(f))]
 		st.write(len(matching_files))
 		for f in matching_files:
-			#st.write(f)
+			st.write(f)
 			path = os.path.join('birds-data/checklistinator', f)
 			area_lists.append(path)
 #			st.write("appended!")
@@ -566,8 +566,9 @@ def get_place(place_str: str):
 					matching_files = [f for f in fs.ls("birds-data/checklistinator") if pattern.match(os.path.basename(f))]
 					st.write(len(matching_files))
 					for f in matching_files:
-						area_lists.append(f)
-						st.write("appended!")
+						path = os.path.join('checklistinator', f)
+						area_lists.append(path)
+						#st.write("appended!")
 					big_area(area_lists)
 				if str(place_original) in states_w_counties:
 					st.write("it's in counties!")
